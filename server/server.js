107057ce117574fe5ec4b02f559e9749ad2d6667
Kishore <kishore1805847@gmail.com>
2026-02-20 16:48:34 +0530

    Day 2: MongoDB connection + User model

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import connectDB from './config/db.js';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+// Connect to MongoDB
+connectDB();
 
 app.get('/', (req, res) => {
   res.json({ message: 'ChatWave API is running 🌊' });
